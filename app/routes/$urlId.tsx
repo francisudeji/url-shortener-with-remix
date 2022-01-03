8e95240ec096b4d74336671ff90ebf67d3a806a1
Francis Udeji <francis.udejiofficial@gmail.com>
2022-01-03 10:07:53 +0100

    Design 404 page

--- app/routes/$urlId.tsx
+++ app/routes/$urlId.tsx
@@ -1,30 +1,40 @@
-import { json, redirect, useLoaderData } from "remix";
-import type { LoaderFunction } from "remix";
+import { json, Link, redirect } from "remix";
+import type { LoaderFunction, MetaFunction } from "remix";
 import { db } from "~/helpers";
 
-export const loader: LoaderFunction = async ({params}) => {
-  const {urlId} = params
+export const meta: MetaFunction = () => {
+  return {
+    title: "404 - Page Not Found | URL Shortener",
+    description: "Page Not Found",
+  };
+};
 
-  const url = await db.url.findUnique({where: { url_id: urlId }})
-  console.log(url)
+export const loader: LoaderFunction = async ({ params }) => {
+  const { urlId } = params;
+
+  const url = await db.url.findUnique({ where: { url_id: urlId } });
 
   if (!url) {
-    return json(urlId, { status: 404, statusText: 'NOT_FOUND' })
+    return json(urlId, { status: 404 });
   }
 
-  return redirect(url.original_url)
-}
+  return redirect(url.original_url);
+};
 
 export default function UrlId() {
-  const data = useLoaderData()
+  return (
+    <main className="flex flex-col items-center justify-center h-screen container mx-auto max-w-lg px-4 space-y-4">
+      <h1 className="text-2xl text-gray-900">Something went wrong!</h1>
 
-  console.log(data);
-  
-  return(
-    <main>
-      <h1>Something went wrong!</h1>
-      
-      <p>The link you've entered is invalid 😔</p>
+      <p className="text-center py-2">
+        The link you're looking for does not exist 😔
+      </p>
+      <Link
+        to="/"
+        className="text-blue-500 text-center hover:underline focus:underline"
+      >
+        Create a short link
+      </Link>
     </main>
-  )
+  );
 }
